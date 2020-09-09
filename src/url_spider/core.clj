89b7016a25691ef13efa215f64b427a39028535f
Ian Sinnott <ian@iansinnott.com>
2020-09-09 11:51:13 +0800

    wip

--- src/url_spider/core.clj
+++ src/url_spider/core.clj
@@ -1,126 +1,115 @@
 (ns url-spider.core
   (:gen-class)
-  (:require [url-spider.second-try :refer [blah]])
+  (:require [clojure.core.async
+             :as a
+             :refer [>! >!! <! <!! go go-loop chan alt!]])
   (:import [org.jsoup Jsoup]))
 
-(defn el->attrs
-  "Extract the attributes of an Element into a plain map"
-  [^org.jsoup.nodes.Element el]
-  (let [attrs (-> el (.attributes))]
-    (reduce (fn [coll x]
-              (assoc coll (keyword (.getKey x)) (.getValue x))) {} attrs)))
+(comment
+  (def -soup (-> "test_doc.html"
+                 (slurp)
+                 (Jsoup/parse))))
 
-(defn el->data
-  "Create plain data structures from a Jsoup element"
-  [^org.jsoup.nodes.Node el]
-  (cond
-    (instance? org.jsoup.nodes.TextNode el) (.text el)
-    (instance? org.jsoup.nodes.Comment el) ""
-    :else (let [tag-name (.tagName el)
-                attrs (el->attrs el)
-                children (->> (.childNodes el)
-                              (map el->data) ;; Recurse
-                              (filter #(not= % "")))] ;; Filter out empty strings
-            [tag-name attrs children])))
+(defn print-chan
+  "Create a channel that will forever print its input"
+  [& args]
+  (let [c (apply chan args)]
+    (go (loop []
+          (println "[IN]" (<! c))
+          (recur)))
+    c))
 
-(defn select
-  [selector soup]
-  (-> soup (.select selector) (->> (map el->data))))
+;; This is rather straightforward. However... it requires a double map to get it
+;; working on a collection of elements it seems
+(defn el->href
+  [el]
+  (-> el (.attr "href")))
+
+(defn mapstuff [coll] (mapcat #(map el->href %) coll))
+
+(def print> (print-chan 1 (mapcat #(map el->href %))))
+(def el-href> (chan 1 (mapcat #(map el->href %))))
+;; (def links (print-chan 1 (mapcat identity)))
+
+
+(defn process-hrefs
+  [input>]
+  (go (loop [visited #{}]
+        (let [next-url (<! input>)]
+          (println "GO ->" next-url)
+          (Thread/sleep 300) ;; TMP While debugging let's throttle this thing a bit
+          (when-not (contains? visited next-url)
+            (println "Should fetch " next-url)
+            (-> next-url
+                (slurp)
+                (Jsoup/parse)
+                (.select "a")
+                (>! input>)))
+          (recur (conj visited next-url))))))
+
+(process-hrefs el-href>)
+
+;; (java.net.URL. "/")
 
 (comment
-  (def -soup
-    (-> "https://iansinnott.com/"
-        (slurp)
-        (Jsoup/parse)))
-  (-> -soup
-      (.select "a")
-      (first)
-      (.childNodes)
-      (nth 2)
-      (el->data))
-  (->> -soup (select "a") (map (comp :href #(second %)))))
+  (-> -soup (.select "a") (first)
+      (el->href))
 
-(defn links-all-pages
-  [x]
-  (let [soup (Jsoup/parse (slurp x))
-        links (->> soup
-                   (select "a")
-                   (map (fn [[_ attrs]] (:href attrs)))
+  (-> -soup (.select "a")
+      (->> (map el->href)))
 
-                   ;; Try to normalize URLs a bit
-                   (map (fn [href] (if (.startsWith href "/") (str x href) href)))
-                   (map (fn [href] (if (not (.contains href "://")) (str x "/" href) href)))
-                   (vec))]
-    links))
+  (def -soup (-> "https://iansinnott.com" (slurp) (Jsoup/parse) (.select "a")))
+  (let [els (-> -soup (.select "a"))]
+    (go (>! el-href> els))))
 
-(comment
-  (links-all-pages "https://lab.iansinnott.com"))
+(let [els (-> "https://iansinnott.com" (slurp) (Jsoup/parse) (.select "a"))]
+  (go (>! print> els)))
 
-(defn queue [x]
-  (conj clojure.lang.PersistentQueue/EMPTY x))
+(eduction (map inc) [1 7 2])
+(def c> (chan))
+(a/put! c> (rand))
+(println (<!! c>))
 
-(defn subdomain?
-  "Is domain a a subdomain of domain b?"
-  [a b]
-  (let [ua (-> a (java.net.URL.) (.getHost))
-        ub (-> b (java.net.URL.) (.getHost))]
-    (.contains ua ub)))
+(go (>! c> (rand)))
 
-(defn same-origin?
-  "Do two URLs have the same host?"
-  [a b]
-  (let [ua (-> a (java.net.URL.) (.getHost))
-        ub (-> b (java.net.URL.) (.getHost))]
-    (= ua ub)))
+(defn drain-chan [c>]
+  (go (loop []
+        (let [x (<! c>)]
+          (if-not (nil? x)
+            (do (println "[drained]" x)
+                (recur))
+            (println "DONE"))))))
+
+(drain-chan c>)
+(drain-chan (a/to-chan! ["hey", "you" "there"]))
+
+(drain-chan (a/merge [(go "sup")
+                      (go "Wee")]))
+
+(def p (promise))
+(deliver p "SUP")
+p
 
 (comment
-  (subdomain?  "https://iansinnott.com/"  "https://iansinnott.com/")
-  (same-origin?  "https://iansinnott.com/"  "https://iansinnott.com/")
-  (subdomain?   "https://blog.iansinnott.com/" "https://iansinnott.com/")
-  (same-origin?   "https://blog.iansinnott.com/" "https://iansinnott.com/")
-  (subdomain?  "https://instagram.com/"  "https://blog.iansinnott.com/")
-  (same-origin?  "https://instagram.com/"  "https://blog.iansinnott.com/"))
-
-(->> ["hey" "ey"]
-     (sort-by count))
-(def include-subdomains false)
-(def root-url "https://iansinnott.com/")
+  (when-not (nil? "a")
+    (println "wee"))
+  (when-some [a nil] ;; When some is a shorthand for (when (not (nil? ...
+    (println "SUP" a))
+  (when-let [a "he"] ;; When let is ALSO... ? a shorthand for (when (not (nil? ...
+    (println "SUP" a)))
 
 (comment
-  (links-all-pages "https://iansinnott.com/")
-  (->> -soup (select "a") (map (fn [[_ attrs]] (:href attrs)))))
+  (def c (chan 30 (mapcat #(map el->href %))))
+  (>!! c (-> -soup (.select "a")))
+  (let [els (-> "https://lab.iansinnott.com" (slurp) (Jsoup/parse) (.select "a"))]
+    (>!! c els))
+  (go (println "GOT" (<! c))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (let [root-url (first args)]
-    (let [state (atom {:visited #{}
-                       :skipped #{}
-                       :failed #{}
-                       :queue (queue root-url)})]
-      (while (not (empty? (:queue @state)))
-        (let [x (peek (:queue @state))
-              visited? (contains? (:visited @state) x)
-              should-skip? (and
-                            (not (same-origin? x root-url))
-                            (not (subdomain? x root-url)))]
-          (try
-            (do
-              (println (str "Maybe... " x))
-              (if should-skip?
-                (swap! state update :skipped conj x)
-                (when (not visited?)
-                  (println (str "DO IT!" x))
-                  (Thread/sleep 100)
-                  (swap! state update :visited conj x)
-                  (let [links (links-all-pages x)]
-                    (swap! state update :queue (fn [y]
-                                                 (apply conj y links))))))
-              (swap! state update :queue pop))
-            (catch Exception e
-              (println "Failed" x)
-              (swap! state update :failed conj x)))))
-      @state)))
+  (let [base-url (first args)]
+    (when (empty? base-url) (throw (Exception. "No URL provided")))))
 
 (comment
   (-main "https://iansinnott.com/"))
--- src/url_spider/second_try.clj
+++ src/url_spider/second_try.clj
@@ -50,11 +50,11 @@
             (recur next))))
     [ping pong c]))
 
-(def _ (pingpong))
-(def ch (last _))
-
-(>!! ch "ping")
-(>!! ch "pong")
+(comment
+  (def _ (pingpong))
+  (def ch (last _))
+  (>!! ch "ping")
+  (>!! ch "pong"))
 
 (let [[ping pong c] (pingpong)]
   (>!! c "ping")
@@ -73,3 +73,125 @@
 (>!! p-machine 1)
 (>!! p-machine 2)
 (>!! p-machine 3)
+
+(defn el->attrs
+  "Extract the attributes of an Element into a plain map"
+  [^org.jsoup.nodes.Element el]
+  (let [attrs (-> el (.attributes))]
+    (reduce (fn [coll x]
+              (assoc coll (keyword (.getKey x)) (.getValue x))) {} attrs)))
+
+(defn el->data
+  "Create plain data structures from a Jsoup element"
+  [^org.jsoup.nodes.Node el]
+  (cond
+    (instance? org.jsoup.nodes.TextNode el) (.text el)
+    (instance? org.jsoup.nodes.Comment el) ""
+    :else (let [tag-name (.tagName el)
+                attrs (el->attrs el)
+                children (->> (.childNodes el)
+                              (map el->data) ;; Recurse
+                              (filter #(not= % "")))] ;; Filter out empty strings
+            [tag-name attrs children])))
+
+(defn select
+  [selector soup]
+  (-> soup (.select selector) (->> (map el->data))))
+
+(comment
+  (def -soup
+    (-> "https://iansinnott.com/"
+        (slurp)
+        (Jsoup/parse)))
+  (-> -soup
+      (.select "a")
+      (first)
+      (.childNodes)
+      (nth 2)
+      (el->data))
+  (->> -soup (select "a") (map (comp :href #(second %)))))
+
+(defn links-all-pages
+  [x]
+  (let [soup (Jsoup/parse (slurp x))
+        links (->> soup
+                   (select "a")
+                   (map (fn [[_ attrs]] (:href attrs)))
+
+                   ;; Try to normalize URLs a bit
+                   (map (fn [href] (if (.startsWith href "/") (str x href) href)))
+                   (map (fn [href] (if (not (.contains href "://")) (str x "/" href) href)))
+                   (vec))]
+    links))
+
+(comment
+  (links-all-pages "https://lab.iansinnott.com"))
+
+(defn queue [x]
+  (conj clojure.lang.PersistentQueue/EMPTY x))
+
+(defn subdomain?
+  "Is domain a a subdomain of domain b?"
+  [a b]
+  (let [ua (-> a (java.net.URL.) (.getHost))
+        ub (-> b (java.net.URL.) (.getHost))]
+    (.contains ua ub)))
+
+(defn same-origin?
+  "Do two URLs have the same host?"
+  [a b]
+  (let [ua (-> a (java.net.URL.) (.getHost))
+        ub (-> b (java.net.URL.) (.getHost))]
+    (= ua ub)))
+
+(comment
+  (subdomain?  "https://iansinnott.com/"  "https://iansinnott.com/")
+  (same-origin?  "https://iansinnott.com/"  "https://iansinnott.com/")
+  (subdomain?   "https://blog.iansinnott.com/" "https://iansinnott.com/")
+  (same-origin?   "https://blog.iansinnott.com/" "https://iansinnott.com/")
+  (subdomain?  "https://instagram.com/"  "https://blog.iansinnott.com/")
+  (same-origin?  "https://instagram.com/"  "https://blog.iansinnott.com/"))
+
+(->> ["hey" "ey"]
+     (sort-by count))
+(def include-subdomains false)
+(def root-url "https://iansinnott.com/")
+
+(comment
+  (links-all-pages "https://iansinnott.com/")
+  (->> -soup (select "a") (map (fn [[_ attrs]] (:href attrs)))))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (let [root-url (first args)]
+    (let [state (atom {:visited #{}
+                       :skipped #{}
+                       :failed #{}
+                       :queue (queue root-url)})]
+      (while (not (empty? (:queue @state)))
+        (let [x (peek (:queue @state))
+              visited? (contains? (:visited @state) x)
+              should-skip? (and
+                            (not (same-origin? x root-url))
+                            (not (subdomain? x root-url)))]
+          (try
+            (do
+              (println (str "Maybe... " x))
+              (if should-skip?
+                (swap! state update :skipped conj x)
+                (when (not visited?)
+                  (println (str "DO IT!" x))
+                  (Thread/sleep 100)
+                  (swap! state update :visited conj x)
+                  (let [links (links-all-pages x)]
+                    (swap! state update :queue (fn [y]
+                                                 (apply conj y links))))))
+              (swap! state update :queue pop))
+            (catch Exception e
+              (println "Failed" x)
+              (swap! state update :failed conj x)))))
+      @state)))
+
+(comment
+  (-main "https://iansinnott.com/"))
